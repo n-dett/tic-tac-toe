fb289ccdba5d2a96eba8c23c89698fb504310e9a
n-dett <124851780+n-dett@users.noreply.github.com>
2024-08-21 19:05:13 -0500

    Add functions to check for win or tie

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -36,7 +36,6 @@ function playerFactory(name, symbol){
 
     // Outer game loop
     do{
-        let gameOver = false;
         let playerTurn = player1;
 
         // Get player names
@@ -47,16 +46,89 @@ function playerFactory(name, symbol){
         let player2 = playerFactory(player2Name, 'O');
 
         do{
+            // Get player's move
             let playerMove = parseInt(prompt((`${playerTurn.name}, which square do you want to select? (1-9)`)));
             gameBoard.gameBoardArr[playerMove-1] = playerTurn.symbol;
 
-        }while(!gameOver);
+            // Switch players for next turn
+            if(!isGameTied() && !isGameWon){
+                if(playerTurn == player1){
+                    playerTurn = player2;
+                }else{
+                    playerTurn = player1;
+                }
+            }
+
+        }while(!isGameTied() && !isGameWon());
+
+        if(isGameTied()){
+            console.log("Tie game!");
+        }else if(isGameWon()){
+            console.log(`${playerTurn} wins!`);
+        }
 
     }while (!playAgain);
 
 
-    function gameOver(){
+    // Check for winner
+    function isGameWon(){
+        let gameWon = false;
+
+        // Check each row of three
+        for(i=0; i<=6; i+=3)
+        {
+            if(gameBoard.gameBoardArr[i] != "" && gameBoard.gameBoardArr[i] == theArray[i+1] && theArray[i+1] == theArray[i+2])
+            {
+                gameWon = true;
+            }
+        }
+    
+        // Check each column of six
+        for(i=0; i<3; i++)
+        {
+            if(gameBoard.gameBoardArr[i] != "" && gameBoard.gameBoardArr[i] == theArray[i+3] && theArray[i+3] == theArray[i+6])
+            {
+                gameWon = true;
+            }
+        }
         
+        // Check the diagonals
+        if((theArray[0] != "" && theArray[0] == theArray[4] && theArray[4] == theArray[8]) || (theArray[2] != "" && theArray[2] == theArray[4] && theArray[4] == theArray[6]))
+        {
+            gameWon = true;
+        }
+    
+        return gameWon;
+    }
+
+    // Check for a tie
+    function isGameTied(){
+        let filled = true;
+        for(i=0; i<gameBoard.gameBoardArr.length; i++){
+            if(gameBoard.gameBoardArr[i] == ''){
+                filled = false;
+            }
+        }
+
+        if(filled){
+            console.log("It's a tie!");
+            return true;
+        }else{
+            return false;
+        }
+
     }
 
 })();
+
+// [][][]
+// [][][]
+// [][][]
+// 123
+// 456
+// 789
+// 159
+// 357
+// 147
+// 258
+// 369
